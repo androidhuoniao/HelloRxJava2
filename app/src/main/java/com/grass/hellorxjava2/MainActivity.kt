510e73748b6d4c5e8933e91526415518867c740e
androidhuoniao <androidhuoniao@hotmail.com>
2020-06-11 22:34:05 +0800

    添加一个简单的测试用例

--- app/src/main/java/com/grass/hellorxjava2/MainActivity.kt
+++ app/src/main/java/com/grass/hellorxjava2/MainActivity.kt
@@ -2,6 +2,7 @@ package com.grass.hellorxjava2
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import io.reactivex.*
 import io.reactivex.disposables.Disposable
@@ -13,7 +14,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//        test()
+        findViewById<TextView>(R.id.start).setOnClickListener {
+            test()
+            test2()
+        }
     }
 
     override fun onResume() {
@@ -22,41 +26,39 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun test() {
-
         Observable.create(object : ObservableOnSubscribe<Int> {
             override fun subscribe(emitter: ObservableEmitter<Int>) {
                 if (!emitter.isDisposed) {
                 }
-                Log.i(TAG, "test is working1")
+                Log.i(TAG, "test is working1 ${emitter.isDisposed}")
                 emitter.onNext(1)
-                Log.i(TAG, "test is working2")
+                Log.i(TAG, "test is working2 ${emitter.isDisposed}")
                 emitter.onNext(2)
-//                emitter.onComplete()
             }
         })
-                .doOnSubscribe(checkAccount())
-                .subscribeOn(Schedulers.io())
-                .compose(uploadLogError(TAG, "同步关注列表出错"))
-                .publish()
-                .autoConnect()
-                .onErrorResumeNext(Observable.empty())
-                .subscribe(object : Observer<Int> {
-                    override fun onComplete() {
-                        Log.i(TAG, "onComplete")
-                    }
+            .doOnSubscribe(checkAccount())
+            .subscribeOn(Schedulers.io())
+            .compose(uploadLogError(TAG, "同步关注列表出错"))
+            .publish()
+            .autoConnect()
+            .onErrorResumeNext(Observable.empty())
+            .subscribe(object : Observer<Int> {
+                override fun onComplete() {
+                    Log.i(TAG, "onComplete")
+                }
 
-                    override fun onSubscribe(d: Disposable) {
-                        Log.i(TAG, "onSubscribe: ")
-                    }
+                override fun onSubscribe(d: Disposable) {
+                    Log.i(TAG, "onSubscribe: ")
+                }
 
-                    override fun onNext(t: Int) {
-                        Log.i(TAG, "onNext: $t")
-                    }
+                override fun onNext(t: Int) {
+                    Log.i(TAG, "onNext: $t")
+                }
 
-                    override fun onError(e: Throwable) {
-                        Log.i(TAG, "onError: $e")
-                    }
-                })
+                override fun onError(e: Throwable) {
+                    Log.i(TAG, "onError: $e")
+                }
+            })
 
 
     }
@@ -71,6 +73,23 @@ class MainActivity : AppCompatActivity() {
      * 记录错误信息到线上日志.
      */
     fun <T> uploadLogError(tag: String?, msg: String): ObservableTransformer<T, T>? {
-        return ObservableTransformer { source -> source.doOnError { throwable -> Log.i(TAG, msg + ": " + throwable.message.toString()) } }
+        return ObservableTransformer { source ->
+            source.doOnError { throwable ->
+                Log.i(
+                    TAG,
+                    msg + ": " + throwable.message.toString()
+                )
+            }
+        }
+    }
+
+    fun test2() {
+        Observable.create<Int> { emitter: ObservableEmitter<Int> ->
+            emitter.onNext(1)
+        }.map {
+            10 * it
+        }.subscribe {
+            Log.i(TAG, "test2: $it")
+        }
     }
 }
